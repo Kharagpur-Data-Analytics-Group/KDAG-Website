e46174598ac4ddd64ecb9b27529ee87c7fb3125f
shauryaraj-kgp <146989609+shauryaraj-kgp@users.noreply.github.com>
2025-02-13 03:34:38 +0530

    Updated Events Gallery Page

--- frontend/src/components/GalleryPage/GalleyPage.js
+++ frontend/src/components/GalleryPage/GalleyPage.js
@@ -3,9 +3,13 @@ import Carousel from './Carousel/Carousel';
 import Header from './Header/Header';
 import './Carousel/css/embla.css';
 
-const OPTIONS = { loop: true };
-const SLIDE_COUNT = 5;
-const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
+const OPTIONS = { loop: true, containScroll: false };
+const SLIDES = [
+  { title: "KDSH 25", image: "images/gallery/kdsh-25/1.jpeg" },
+  { title: "KDSH 24", image: "images/gallery/kdsh-24/10.jpg" },
+  { title: "KDSH 25", image: "images/gallery/kdsh-25/1.jpeg" },
+  { title: "KDSH 24", image: "images/gallery/kdsh-24/10.jpg" }
+];
 
 const GalleryPage = () => {
   return (
