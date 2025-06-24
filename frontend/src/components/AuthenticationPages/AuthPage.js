416138eb2678e18a36fa6ae0f32132d8d05ba92a
shauryaraj-kgp <146989609+shauryaraj-kgp@users.noreply.github.com>
2025-06-25 05:18:59 +0530

    fix: backend login issues

--- frontend/src/components/AuthenticationPages/AuthPage.js
+++ frontend/src/components/AuthenticationPages/AuthPage.js
@@ -47,7 +47,7 @@ const AuthPage = () => {
 	const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
 
 	function handleGoogleAuth() {
-		const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&access_type=offline&scope=email&prompt=consent`;
+		const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&access_type=offline&scope=email%20profile%20openid&prompt=consent`;
 		window.location.href = googleAuthUrl;
 	}
 
