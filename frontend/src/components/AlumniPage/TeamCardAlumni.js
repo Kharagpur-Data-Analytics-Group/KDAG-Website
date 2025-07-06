f6557b5cdf1ca809d52f3fe6921ea7f33ebea87b
Varsham <164515113+CodeInfinity1@users.noreply.github.com>
2025-07-06 11:19:45 +0530

    Updated Teams Page

--- frontend/src/components/AlumniPage/TeamCardAlumni.js
+++ frontend/src/components/AlumniPage/TeamCardAlumni.js
@@ -22,16 +22,19 @@ const TeamCardAlum = ({ member }) => {
 						<div class="alumni-member-follow-text">
 							<span>Follow on</span>
 						</div>
-						<div class="alumni-member-social-icon"> 
-							{/* <div class="alumni-member-social-icon-icon">
-								<a
-									href={member?.facebook || "#"}
-									target="_blank"
-									rel="noreferrer noopener"
-								>
-									<i class="fab fa-facebook"></i>
-								</a>
-							</div> */}
+						<div class="alumni-member-social-icon">
+							<div class="alumni-member-social-icon-icon">
+								{member?.facebook && (
+									<a
+										href={member.facebook}
+										target="_blank"
+										rel="noreferrer noopener"
+									>
+										<i className="fab fa-facebook"></i>
+									</a>
+								)}
+
+							</div>
 
 							<div class="alumni-member-social-icon-icon">
 								<a
