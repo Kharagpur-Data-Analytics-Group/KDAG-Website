0b67d2ca2f3ec2967f55323f55dc44c17b2405dd
bothrayash276 <bothrayash276@gmail.com>
2026-06-20 12:23:44 +0530

    alumni header built

--- newfrontend/src/components/AlumniPage/Header2.jsx
+++ newfrontend/src/components/AlumniPage/Header2.jsx
@@ -16,17 +16,17 @@ const Header2 = () => {
 
   return (
     <>
-    <div className={`!h-120 !pt-40 bg-cover shadow-[0px_2px_10px_rgba(0,0,0,0.25)] transition-opacity duration-1000 ${isVisible ? "opacity-100" : "opacity-0"}`}
-    >
+    <div className={`h-120 bg-cover shadow-[0px_2px_10px_rgba(0,0,0,0.25)] transition-opacity duration-1000 ${isVisible ? "opacity-100" : "opacity-0"}`}
+    style={{ paddingTop: '10rem'}}>
 			<div 
-            className='font-[Poppins, sans-serif] text-[4rem] font-bold text-center text-white'
-            >
+                className='font-[Poppins, sans-serif] text-[4rem] font-bold text-center text-white'
+                style={{fontFamily : 'Poppins, sans-serif'}}>
                 OUR ALUMNI NETWORK
             </div>
 
 			<div
-                className='font-[Poppins, sans-serif] text-[1.2rem] text-center text-[#ddd] w-1/2 min-w-120 m-auto'
-			>
+                className='text-[1.2rem] text-center text-[#ddd] w-1/2 min-w-120 m-auto'
+			    style={{fontFamily : 'Poppins, sans-serif'}}>
 				Meet the distinguished alumni of Kharagpur Data Analytics Group and discover the impact they've made in the world of Machine Learning and Artificial Intelligence.
 			</div>
 		</div>
